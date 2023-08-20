1781ae8802fc43ea0f926cf0795716d9e47e8762
tjwilks <tobywilkinson3@gmail.com>
2023-08-20 21:02:59 +0100

    Forecast with UIRP model

--- main.py
+++ main.py
@@ -1,29 +1,44 @@
 import configparser
 import os
 import sys
-from ForexForcasting.data_loader import ForexLoader, ConfigLoader
+
+import pandas as pd
+
+from ForexForcasting.data_loader import ForexLoader, ConfigLoader, InterestRateLoader
 from ForexForcasting.backtesting import TimeseriesBacktestDataset
 from ForexForcasting.backtesting import Backtester, BacktestWindow
-from ForexForcasting.models import RandomWalk, ARIMA
+from ForexForcasting.models import RandomWalk, ARIMA, UIRPForecaster
 from ForexForcasting.results import ForecastResults
 from ForexForcasting.preprocessing import Preprocessor
+pd.set_option('display.max_columns', None)  # Set to None to display all columns
+pd.set_option('display.width', 1000000)  # Set to None to display all columns
+
 
 def main(main_config):
     forex_loader = ForexLoader()
+    interest_rate_loader = InterestRateLoader()
     data = forex_loader.load(
         source_type=main_config['general']['source_type'],
-        path=main_config['general']['path']
+        path=main_config['general']['forex_data_path']
     )
+    interest_rate_data = interest_rate_loader.load(
+        source_type=main_config['general']['source_type'],
+        path=main_config['general']['interest_rate_data_path']
+    )
+    data = data.merge(interest_rate_data, on=["currency_pair", 'date'])
     data = data[data["currency_pair"] == "USD/COP"]
-    data = data[data['Date'].dt.year > 2015]
+    data = data[data['date'].dt.year > 2015]
     backtest_dataset = TimeseriesBacktestDataset(
-        dates=data['Date'].to_list(),
+        dates=data['date'].to_list(),
         y_data=data['Close'].to_list(),
         window=BacktestWindow(
             n_obs=len(data),
             train_window_len=52,
             max_test_window_length=1
-        )
+        ),
+        regressor_data={
+            "interest_rate_delta": data['interest_rate_delta'].to_list()
+        }
     )
     config_loader = ConfigLoader()
     preprocessing_config = config_loader.load(
@@ -33,7 +48,7 @@ def main(main_config):
     models = [
         RandomWalk(),
         ARIMA(1, 1, 0),
-        ARIMA(2, 1, 0)
+        UIRPForecaster()
     ]
     backtester = Backtester(
         backtest_dataset=backtest_dataset,
@@ -43,7 +58,7 @@ def main(main_config):
     )
     backtester.run()
     backtester.results.plot_error_over_time(
-        list(range(365))
+        list(range(len(backtester.results.forecast_datasets)))
     )
     backtester.results.calculate_mean_model_error(
         list(range(len(backtester.results.forecast_datasets)))
